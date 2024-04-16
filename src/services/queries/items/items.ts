07ceb4e07051a0bedb35515f59f8259425d48986
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2024-04-16 10:19:34 +0300

    feat: add getUer and getItems service

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,39 @@
 import type { CreateItemAttrs } from '$services/types';
+import { client } from '$services/redis';
+import { serialize } from './serialize';
+import { genId } from '$services/utils';
+import { itemsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+    const item = await client.hGetAll(itemsKey(id));
 
-export const getItems = async (ids: string[]) => {};
+    if (Object.keys(item).length === 0) return null
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+    return deserialize(id, item);
+};
+
+export const getItems = async (ids: string[]) => {
+    const commands = ids.map((id) => {
+        return client.hGetAll(itemsKey(id));
+    });
+
+
+    const results = await Promise.all(commands);
+
+    return results.map((result, idx) => {
+        if (Object.keys(result).length === 0) return null
+
+        return deserialize(ids[idx], result);
+    })
+};
+
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+    const id = genId();
+
+    const serialized = serialize(attrs);
+
+    await client.hSet(itemsKey(id), serialized)
+
+    return id;
+};
