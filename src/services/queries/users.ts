a93d2f4c021284a1a5a11e70b8a51a26600f773f
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2024-04-22 22:45:22 +0300

    feat: add getUserByUsername

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,9 +1,20 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usernamesUniqueKey, usersKey } from '$services/keys';
+import { userNamesKey, usernamesUniqueKey, usersKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+    // Use the username arg to look up the persons User ID with the usernamse sorted set
+    const userIdRaw = await client.zScore(userNamesKey(), username)
+    // make sure we actually got an ID from lookup1
+    if(!userIdRaw) throw new Error('User does not exist !');
+    // Take the ID conver it back
+    const userId = userIdRaw.toString(16)
+    // Look up the user hash
+    const user = await client.hGetAll(usersKey(userId))
+    // deserialize and return the hash
+    return deserialize(userId, user)
+};
 
 export const getUserById = async (id: string) => {
     const user = await client.hGetAll(usersKey(id))
@@ -19,6 +30,11 @@ export const createUser = async (attrs: CreateUserAttrs) => {
     await client.hSet(usersKey(id), serialize(attrs))
     //Add username into usernames
     await client.sAdd(usernamesUniqueKey(), attrs.username)
+    // Add user id into 'usernames' hash
+    await client.zAdd(userNamesKey(), {
+        value: attrs.username,
+        score: parseInt(id, 16)
+    })
 
     return id;
 };
