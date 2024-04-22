7c3674595a2e4deb5bee9b9ca82a893ecd43e8a9
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2024-04-22 21:43:11 +0300

    feat: add like feature

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,36 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { userLikesKey, itemsKey } from "$services/keys";
+import { client } from "$services/redis";
+import { getItem, getItems } from "./items";
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return await client.sIsMember(userLikesKey(userId), itemId)
+};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likedItems = async (userId: string) => {
+    //Fetch all the ids of the items that user like
+    const ids = await client.sMembers(userLikesKey(userId))
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+    // get all the items of these ids 
+    return await getItems(ids);
+};
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    const inserted  = await client.sAdd(userLikesKey(userId), itemId)
+    if(inserted){
+        return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+    }
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId); 
+
+    if(removed) {
+        return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+    }
+};
+
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+
+    return getItems(ids);
+};
