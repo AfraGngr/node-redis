b04cc125ed3fcfa47a13e7b6d58410ea8ebfaaf9
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2024-05-11 17:27:07 +0300

    feat: add most viewed items

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,35 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { itemsByViewKey, itemsKey } from "$services/keys";
+import { client } from "$services/redis";
+import { deserialize } from "./deserialize";
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+    let results = await client.sort(
+        itemsByViewKey(),
+        {
+            GET: [
+                '#', 
+                `${itemsKey('*')}->name`, 
+                `${itemsKey('*')}->views`,
+                `${itemsKey('*')}->endingAt`,
+                `${itemsKey('*')}->imageUrl`,
+                `${itemsKey('*')}->price`
+            ],
+            BY: 'nosort',
+            DIRECTION: order,
+            LIMIT: {
+                offset,
+                count
+            }
+        }
+    ) as unknown as string[]
+
+    const items = [];
+    while (results.length) {
+        const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+        const item = deserialize(id, { name, views, endingAt, imageUrl, price});
+        items.push(item);
+        results = rest
+    }
+
+    return items;
+};
