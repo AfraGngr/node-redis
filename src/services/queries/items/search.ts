8bd15482c3e34539875a6d61e5760e01a742c34d
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2024-07-06 00:14:28 +0300

    feat: create search function for indexes

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,33 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { itemsIndexKey } from "$services/keys";
+import { client } from "$services/redis";
+import { describe } from "pm2";
+import { deserialize } from "./deserialize";
+
+export const searchItems = async (term: string, size: number = 5) => {
+  const cleaned = term
+    .replaceAll(/[^a-zA-Z0-9]/g, '')
+    .trim()
+    .split(' ')
+    .map(word =>  word ? `%${word}%` : '')
+    .join()
+
+    // Look at cleaned and make sure it is valid
+    if (cleaned === '')  return [];
+
+    // Use the client to do an actual search
+    const results = await client.ft.search(
+        itemsIndexKey(),
+        cleaned,
+        {
+            LIMIT : {
+                from : 0,
+                size
+            }
+        }
+
+    )
+
+    console.log(results);
+    // Deserialize and return search result
+    results.documents.map(({id, value}) => deserialize(id, value as any))
+};
